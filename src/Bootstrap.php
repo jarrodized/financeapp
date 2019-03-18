ae94e2d187a8414797af1c7c7f895f0ae5bf5bc5
jarrodized <jarrodized@yahoo.com>
2019-03-18 17:28:23 -0400

    Add basic request and response handling

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -21,4 +21,20 @@ if ($environment !== 'production') {
 }
 $whoops->register();
 
-throw new \Exception;
+/**
+ * Register the request and response
+ */
+$request = new \Http\HttpRequest($_GET, $_POST, $_COOKIE, $_FILES, $_SERVER);
+$response = new \Http\HttpResponse;
+
+$content = '<h1>Hello World</h1>';
+$response->setContent($content);
+
+// $response->setContent('404 - Page not found');
+// $response->setStatusCode(404);
+
+foreach ($response->getHeaders() as $header) {
+  header($header, false);
+}
+
+echo $response->getContent();
